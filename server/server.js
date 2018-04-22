ae8814bacbe29aeeecbe95b59ee34b6443527ea9
antiproton85 <kevinmcgr@gmail.com>
2018-04-22 15:25:08 -0400

    GCP ENV madness

--- server/server.js
+++ server/server.js
@@ -1,12 +1,33 @@
 // Adding automatic tracing through the TraceAgent
 if (process.env.NODE_ENV === 'production') {
 	require('@google-cloud/trace-agent').start()
+	require('@google-cloud/debug-agent').start({ allowExpressions: true });
 }
+
+/*
 else {
 	require('@google-cloud/trace-agent').start({
 		projectId: 'symphony-gamma-poc',
 		keyFilename: './credentials/symphony-gamma-poc-493624b6cca1.json'
 	})
+
+	require('@google-cloud/debug-agent').start({
+		allowExpressions: true,
+  		projectId: 'symphony-gamma-poc',
+  		keyFilename: './credentials/symphony-gamma-poc-493624b6cca1.json'
+});
+}*/
+
+console.log(`Are the ENV Variables working?: ${process.env.SFDC_API_KEY}`)
+
+let serverState = process.env.NODE_ENV || 'development'
+console.log(`Server State detected as: ${serverState}`)
+
+if (process.env.NODE_ENV === 'production') {
+	console.log('Initializing server - Production')
+}
+else {
+	console.log('Initializing server - development')
 }
 
 const { Nuxt, Builder } = require('nuxt')
@@ -42,8 +63,14 @@ app.use(nuxt.render)
 
 //Or here. nuxt.render is technically middleware, but it's not using the express server
 
+const appPort = process.env.PORT || 8080
 
+console.log(`app.yaml reports listening port: ${process.env.PORT}`)
+console.log(`Setting server lisetning port to: ${appPort}`)
 
-app.listen(8081)
+app.listen(appPort, () => {
+    const port = server.address().port;
+    console.log(`App listening on port ${port}`);
+  })
 
-console.log('Custom Server is listening on http://localhost:8081')
+//console.log('Custom Server is listening on http://localhost:8081')
